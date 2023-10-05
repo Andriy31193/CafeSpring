35e1283de41ee58b74d3cff70cade7cd9bccc3ee
Andriy31193 <andriycreator@gmail.com>
2023-10-05 23:51:15 +0000

    Update V2_0_2__addWorkSchedule.sql

--- src/main/resources/db/migration/V2_0_2__addWorkSchedule.sql
+++ src/main/resources/db/migration/V2_0_2__addWorkSchedule.sql
@@ -1,5 +1,5 @@
 INSERT INTO staff (name, position_id, workschedule_id)
-VALUES ('Лёша', 2, 7);
+VALUES ('Jack', 2, 7);
 
 INSERT INTO staff (name, position_id, workschedule_id)
-VALUES ('Олексий', 3, 1);
+VALUES ('Alex', 3, 1);
